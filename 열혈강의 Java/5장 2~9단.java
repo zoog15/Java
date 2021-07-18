e4c3e8397163269be6a8001a693c3b1fd397dd59
kimminkwan <zoog15@naver.com>
2021-07-18 19:16:40 +0900

    Create 5장 2~9단.java

---
+++ 열혈강의 Java/5장 2~9단.java
@@ -0,0 +1,12 @@
+public class gugudan {
+	public static void main(String[] args) {
+		for(int i = 2;i<10;i++)
+		{
+			for(int j = 1;j<10;j++)
+			{
+				System.out.print(i+"*"+j+"="+i*j+" ");
+			} //end for j
+			System.out.println(); // ±×³É ÁÙ¹Ù²Þ
+		} //end for i
+	}
+}
