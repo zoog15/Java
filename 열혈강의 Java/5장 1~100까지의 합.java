1c92e4c45c0da33c6fa446e709184063f3397eb8
kimminkwan <zoog15@naver.com>
2021-07-18 19:16:39 +0900

    Create 5장 1~100까지의 합.java

---
+++ 열혈강의 Java/5장 1~100까지의 합.java
@@ -0,0 +1,12 @@
+public class sum {
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		for(int i= 1;i<=100;i++)
+		{
+			sum += i;
+		}
+		
+		System.out.println("1부터 100까지의 합 :"+sum);
+	}
+}
