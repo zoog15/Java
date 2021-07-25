5ca9940c4f02921949d4a372bf555637818c934b
KimMinKwan <zoog15@naver.com>
2021-07-25 19:50:19 +0900

    Create Ball.java
    super의 사용법

---
+++ 열혈강의 Java/chap10/Heritance3/Ball.java
@@ -0,0 +1,13 @@
+package chap10.Heritance3;
+
+public class Ball {
+	private double radius;
+	
+	public Ball(double radius) {
+		this.radius = radius;
+	}
+	
+	public String toString() {
+		return "이 공의 반지름은" + radius;
+	}
+}
