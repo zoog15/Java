0b0e32396f70a9d21cc1b725642a8acdfee0d47b
KimMinKwan <zoog15@naver.com>
2021-07-25 19:49:48 +0900

    Create Mouse.java
    마우스 클릭

---
+++ 열혈강의 Java/chap10/Heritance2/Mouse.java
@@ -0,0 +1,10 @@
+package chap10.Heritance2;
+
+public class Mouse {
+	public void clickLeft() {
+		System.out.println("왼쪽 클릭");
+	}
+	public void clickRight() {
+		System.out.println("오른쪽 클릭");
+	}
+}
