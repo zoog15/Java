d28ebcc5027111562e27814b391df6d1cff3879c
KimMinKwan <zoog15@naver.com>
2021-07-28 13:29:12 +0900

    Create KimbobNara.java

---
+++ 열혈강의 Java/chap11/KimbobHeaven/KimbobNara.java
@@ -0,0 +1,21 @@
+package chap11.KimbobHeaven;
+
+public class KimbobNara implements BunSikHouse{
+
+	@Override
+	public void makeKimBob() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void makeDukBokGi() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void makeInstanceNoodle() {
+		// TODO Auto-generated method stub
+	}
+}
