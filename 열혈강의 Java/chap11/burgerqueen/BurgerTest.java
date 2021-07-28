a1972b97470d16b60533d94558069e3506eb981f
KimMinKwan <zoog15@naver.com>
2021-07-28 13:28:55 +0900

    Create BurgerTest.java

---
+++ 열혈강의 Java/chap11/burgerqueen/BurgerTest.java
@@ -0,0 +1,9 @@
+package chap11.burgerqueen;
+
+public class BurgerTest {
+	public static void main(String[] args) {
+		BurgerClerk cook = new BurgerClerk();
+		cook.orderBurger();
+		cook.orderSalad();
+	}
+}
