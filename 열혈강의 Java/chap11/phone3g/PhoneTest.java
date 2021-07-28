3d44dfb59e3fd29e592ca69be0cc132b5c5d0807
KimMinKwan <zoog15@naver.com>
2021-07-28 13:29:26 +0900

    Create PhoneTest.java

---
+++ 열혈강의 Java/chap11/phone3g/PhoneTest.java
@@ -0,0 +1,9 @@
+package chap11.phone3g;
+
+public class PhoneTest {
+	public static void main(String[] args) {
+		Phone3G1 phone = new Phone3G1();
+		VisualCall v1 = new Phone3G1();
+		VoiceCall v2 = new Phone3G1(); // 전부 선언 가능
+	}
+}
