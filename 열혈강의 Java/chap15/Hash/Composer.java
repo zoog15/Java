4f0ad2bb2c0156578eecf3ba73fe9a8fdc8f05e4
KimMinKwan <zoog15@naver.com>
2021-07-29 23:19:39 +0900

    Create Composer.java

---
+++ 열혈강의 Java/chap15/Hash/Composer.java
@@ -0,0 +1,18 @@
+package chap15.Hash;
+
+public class Composer {
+	private String name;
+	private String nation;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNation() {
+		return nation;
+	}
+	public void setNation(String nation) {
+		this.nation = nation;
+	}
+}
