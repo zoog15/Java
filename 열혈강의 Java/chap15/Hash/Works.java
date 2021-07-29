24cdfdbbe3c1486ab655c19e971d5c8d52d20f23
KimMinKwan <zoog15@naver.com>
2021-07-29 23:19:47 +0900

    Create Works.java

---
+++ 열혈강의 Java/chap15/Hash/Works.java
@@ -0,0 +1,25 @@
+package chap15.Hash;
+
+public class Works {
+	private String type;
+	private String title;
+	private String year;
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+}
