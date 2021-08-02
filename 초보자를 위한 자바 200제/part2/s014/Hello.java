0ba841fb6c1efc583f108c756ec6356324de7a09
KimMinKwan <zoog15@naver.com>
2021-08-02 23:05:25 +0900

    S14 자바 프로그래밍 실행하기

---
+++ 초보자를 위한 자바 200제/part2/s014/Hello.java
@@ -0,0 +1,11 @@
+package kr.co.infopub.chapter.s014; // 패키지는 클래스가 어디에 포함되는지 알려줌
+
+public class Hello { // Hello 클래스 선언
+	public static void main(String[] args) { // 프로그래밍을 실행시키는 메서드
+
+		// 콘솔에 "" 사이 내용을 표시. println()은 출력후 다음 줄 이동
+		System.out.println("*******************************************");
+		System.out.println("*     이 프로그래밍은 Java200이 만들었습니다.     *");
+		System.out.println("*******************************************");
+	}
+}
