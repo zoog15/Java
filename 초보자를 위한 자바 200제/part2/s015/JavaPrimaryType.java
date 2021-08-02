e14f2e2f4ac4dfea93ea3d29fc024322d53d9e62
KimMinKwan <zoog15@naver.com>
2021-08-02 23:04:57 +0900

    s16 API에서 제공하는 참조 타입 사용하기

---
+++ 초보자를 위한 자바 200제/part2/s015/JavaPrimaryType.java
@@ -0,0 +1,12 @@
+package kr.co.infopub.chapter.s015;
+//기본 타입의 종류
+public class JavaPrimaryType {
+	public static void main(String[] args) {
+		boolean isL = false; // 참거짓을 나타내는 boolean.
+		char suit = 'H'; // 문자 H를 suit에 저장
+		int year = 2016; // 소수점 없는 정수
+		double latitude = 37.52127220511242;
+		double longitude = 127.0074462890625;
+		System.out.println(latitude+"\t"+longitude);
+	}
+}
