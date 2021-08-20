14e1db0219b0aee2700e7e56007f138dc82b18ec
KimMinKwan <zoog15@naver.com>
2021-08-20 11:02:31 +0900

    54. 향상된 for(foreach)로 배열 출력하기

---
+++ 초보자를 위한 자바 200제/part3/s054/ForeachLotto.java
@@ -0,0 +1,12 @@
+package kr.co.infopub.chapter.s054;
+
+// 향상된 for와 배열
+public class ForeachLotto {
+	public static void main(String[] args) {
+		int[] mm = {1,6,16,22,23,33};
+		for (int m : mm) {
+			System.out.printf(m+"\t");
+		}
+		System.out.println();
+	}
+}
