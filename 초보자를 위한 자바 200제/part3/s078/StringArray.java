b62589b9c0f06f463ea099076391670ba81a0986
KimMinKwan <zoog15@naver.com>
2021-08-22 21:02:21 +0900

    78. 문자열 1차원 배열 이해

---
+++ 초보자를 위한 자바 200제/part3/s078/StringArray.java
@@ -0,0 +1,21 @@
+package kr.co.infopub.chapter.s078;
+
+public class StringArray {
+	public static void main(String[] args) {
+		String[] myNum = new String[] {"2016-08-17", "2016-09-17", "2016-03-17"};
+		String[] myNum2 = {"2016-08-17", "2016-09-17", "2016-03-17"};
+		System.out.println(myNum2[0]);
+		print(myNum);
+		
+		myNum = new String[] {"2016-08-17", "2016-09-17", "2016-03-17", "2016-04-17"};
+		String[] tos = new String[myNum.length];
+		System.arraycopy(myNum, 0, tos, 0, myNum.length);
+		print(tos);
+	}
+	public static void print(String[] tos) {
+		for (String ss: tos) {
+			System.out.printf(ss+" ");
+		}
+		System.out.println();
+	}
+}
