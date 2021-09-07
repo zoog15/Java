da0156d0d08de039dd8b56a6d688fbc51510407d
KimMinKwan <zoog15@naver.com>
2021-09-07 18:04:53 +0900

    105. equals overriding을 이용해 카드값이 같으면 같은 객체로 만들기

---
+++ 초보자를 위한 자바 200제/part4/s105/CardMain2.java
@@ -0,0 +1,14 @@
+package kr.co.infopub.chapter.s105;
+
+public class CardMain2 {
+	public static void main(String[] args) {
+		Card c1 = new Card("H4");
+		Card c2 = new Card("H4");
+		
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c1.getCardVal());
+		System.out.println(c2.getCardVal());
+		System.out.println(c1.equals(c2));
+	}
+}
