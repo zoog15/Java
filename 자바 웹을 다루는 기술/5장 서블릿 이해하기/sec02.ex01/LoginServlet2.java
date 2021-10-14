1b5052a2b68497bc265a8089858f927857614f5e
KimMinKwan <zoog15@naver.com>
2021-10-14 20:13:22 +0900

    서블릿 기초

---
+++ 자바 웹을 다루는 기술/5장 서블릿 이해하기/sec02.ex01/LoginServlet2.java
@@ -0,0 +1,45 @@
+package pro06.sec02.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login2")
+public class LoginServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init 메서드 호출");
+	}
+
+	public void destroy() {
+		System.out.println("destroy 메서드 호출");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 웹 브라우저에게 전송될 데이터의 인코딩 설정
+		request.setCharacterEncoding("utf-8");
+		// 응답할 데이터 종류가 HTML임을 설정
+		response.setContentType("text/html; charset=utf-8");
+		// HttpServletResponse 객체의 getWriter()을 이용해 출력 스트림 PrintWriter 객체 받아오기
+		PrintWriter out = response.getWriter();
+		
+		String id = request.getParameter("user_id");
+		String pw = request.getParameter("user_pw");
+		
+		String data = "<html>";
+		data += "<body>";
+		data += "아이디 : " + id;
+		data += "<br>";
+		data += "패스워드 : " + pw;
+		data += "</body>";
+		data += "</html>";
+		out.print(data);
+	}
+}
