335ec314c682c6f28c58644d172eec3376825d4b
KimMinKwan <zoog15@naver.com>
2021-10-16 15:50:29 +0900

    PreparedStatement 인터페이스를 통한 DB 연동 성능 개선

---
+++ 자바 웹을 다루는 기술/7장 서블릿 비즈니스 로직 처리/sec01.ex02/MemberDAO.java
@@ -0,0 +1,64 @@
+package sec01.ex02;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberDAO {
+	private static final String driver = "oracle.jdbc.driver.OracleDriver";
+	private static final String url = "jdbc:oracle:thin:@localhost:1521:XE";
+	private static final String user = "scott";
+	private static final String pwd = "tiger";
+	private Connection con;
+	private Statement pstmt;
+	
+	public List<MemberVO> listMembers(){
+		List<MemberVO> list = new ArrayList<MemberVO>();
+		try {
+			connDB();
+			String query = "select * from t_member";
+			System.out.println("prepareStatement: " + query);
+			// prepaerStatement() 메서드에서 SQL문을 전달해 PreparedStatement 객체 생성
+			pstmt = con.prepareStatement(query);
+			// executeQuery() 메서드를 호출해 미리 설정한 SQL문 실행
+			ResultSet rs = pstmt.executeQuery(query);
+			
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String pwd = rs.getString("pwd");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				Date joinDate = rs.getDate("joinDate");
+				MemberVO vo = new MemberVO();
+				vo.setId(id);
+				vo.setPwd(pwd);
+				vo.setName(name);
+				vo.setEmail(email);
+				vo.setJoinDate(joinDate);
+				list.add(vo);
+			}
+			rs.close();
+			pstmt.close();
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return list;
+	}
+
+	private void connDB() {
+		try {
+			Class.forName(driver);
+			System.out.println("Oracle 드라이버 로딩 성공");
+			con = DriverManager.getConnection(url, user, pwd);
+			System.out.println("Connection 생성 성공");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ 자바 웹을 다루는 기술/7장 서블릿 비즈니스 로직 처리/sec01.ex02/MemberServlet.java
@@ -0,0 +1,41 @@
+package sec01.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member")
+public class MemberServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		// SQL문으로 조회할 MemberDAO 객체 생성
+		MemberDAO dao = new MemberDAO(); 
+		// listMembers) 메서드로 회원 정보를 조회
+		List<MemberVO> list = dao.listMembers();
+		
+		out.print("<html><body>");
+		out.print("<table border=1><br align='center' bgcolor='lightgreen'>");
+		out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일</td></tr>");
+		
+		for (int i = 0; i < list.size(); i++) {
+			MemberVO memberVO = (MemberVO)list.get(i);
+			String id = memberVO.getId();
+			String pwd = memberVO.getPwd();
+			String name = memberVO.getName();
+			String email = memberVO.getEmail();
+			Date joinDate = memberVO.getJoinDate();
+			out.print("<tr><td>" + id + "</td><td>" + pwd + "</td><td>" + name + "</td><td>" + email + "</td><td>" + joinDate + "</td><tr>");
+		}
+		out.print("</table></body></html>");
+	}
+}
