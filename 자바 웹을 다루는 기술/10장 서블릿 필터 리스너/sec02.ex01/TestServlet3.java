2aa714c87459e62acb0dd3f6e3ff529f9ed01ff9
KimMinKwan <zoog15@naver.com>
2021-10-22 15:43:37 +0900

    서블릿의 여러가지 URL 패턴

---
+++ 자바 웹을 다루는 기술/10장 서블릿 필터 리스너/sec02.ex01/TestServlet3.java
@@ -0,0 +1,45 @@
+package sec02.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// 확장자만 일치하는 패턴
+@WebServlet("*.do")
+public class TestServlet3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		// 컨텍스트 이름만 가져옴
+		String context = request.getContextPath();
+		// 전체 URL 가져옴
+		String url = request.getRequestURL().toString();
+		// 서블릿 매핑 이름만 가져옴
+		String mapping = request.getServletPath();
+		// URI를 가져옴
+		String uri = request.getRequestURI();
+		
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Test Servlet1</title>");
+		out.println("</head>");
+		out.println("<body bgcolor='red'>");
+		out.println("<b>TestServlet3입니다.</b><br>");
+		out.println("<b>컨텍스트 이름 : " + context + "</b><br>");
+		out.println("<b>전체 경로 : " + url + "<b><br>");
+		out.println("<b>매핑 이름 : " + mapping + "<b><br>");
+		out.println("<b>URI : " + uri + "<b>");
+		out.println("</body>");
+		out.println("</html>");
+		out.close();
+	}
+}
