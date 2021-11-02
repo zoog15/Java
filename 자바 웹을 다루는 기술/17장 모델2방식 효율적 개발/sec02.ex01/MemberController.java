2964f55564f16bf9bdf12bcbee2c4e5378156b6a
KimMinKwan <zoog15@naver.com>
2021-11-02 11:19:50 +0900

    MVC패턴 회원 추가기능

---
+++ 자바 웹을 다루는 기술/17장 모델2방식 효율적 개발/sec02.ex01/MemberController.java
@@ -0,0 +1,63 @@
+package sec02.ex01;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/member/*")
+public class MemberController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	MemberDAO memberDAO;
+
+	public void init(ServletConfig config) throws ServletException {
+		memberDAO = new MemberDAO();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nextPage = null;
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		// URL에서 요청명 가져옴
+		String action = request.getPathInfo();
+		System.out.println("action:" + action);
+		
+		// 최초 요청이거나 action 값이 /memberList.do면 회원 목록 출력
+		if (action == null || action.equals("/listMembers.do")) {
+			List<MemberVO> membersList = memberDAO.listMembers();
+			request.setAttribute("membersList", membersList);
+			// test02 폴더의 listMember.jsp로 포워딩
+			nextPage = "/test02/listMembers.jsp";
+		} else if (action.equals("/addMember.do")) { // action 값이 /addMember.do면 전송된 회원 정보를 테이블에 추가
+			String id = request.getParameter("id");
+			String pwd = request.getParameter("pwd");
+			String name = request.getParameter("name");
+			String email = request.getParameter("email");
+			MemberVO memberVO = new MemberVO(id, pwd, name, email);
+			memberDAO.addMember(memberVO);
+			// 회원 등록 후 다시 목록 출력
+			nextPage ="/member/listMembers.do";
+		} else if (action.equals("/memberForm.do")) {
+			nextPage = "/test02/MemberForm.jsp";
+		} else { // 그외 다른 값은 회원 목록 출력
+			List<MemberVO> membersList = memberDAO.listMembers();
+			request.setAttribute("membersList", membersList);
+			nextPage = "/test02/listMembers.jsp";
+		}
+		RequestDispatcher dispatch = request.getRequestDispatcher(nextPage);
+		dispatch.forward(request, response);
+	}
+}
