a097c14f1756c322c627e3adc73b042c8a956672
KimMinKwan <zoog15@naver.com>
2021-11-08 23:13:05 +0900

    모델2 답변형 게시판 : 게시판 답글달기 기능

---
+++ 자바 웹을 다루는 기술/17장 모델2방식 효율적 개발/sec03.brd07/BoardController.java
@@ -0,0 +1,234 @@
+package sec03.brd07;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletRequestContext;
+
+
+@WebServlet("/board/*")
+public class BoardController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private static String ARTICLE_IMAGE_REPO = "D:\\board\\article_image";
+	BoardService boardService;
+	ArticleVO articleVO;
+
+	public void init(ServletConfig config) throws ServletException {
+		boardService = new BoardService();
+		articleVO = new ArticleVO();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)	throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nextPage = "";
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		HttpSession session;
+		String action = request.getPathInfo();
+		System.out.println("action:" + action);
+		try {
+			List<ArticleVO> articlesList = new ArrayList<ArticleVO>();
+			if (action == null) {
+				articlesList = boardService.listArticles();
+				request.setAttribute("articlesList", articlesList);
+				nextPage = "/board06/listArticles.jsp";
+			} else if (action.equals("/listArticles.do")) {
+				articlesList = boardService.listArticles();
+				request.setAttribute("articlesList", articlesList);
+				nextPage = "/board06/listArticles.jsp";
+			} else if (action.equals("/articleForm.do")) {
+				nextPage = "/board06/articleForm.jsp";
+			} else if (action.equals("/addArticle.do")) {
+				int articleNO=0;
+				Map<String, String> articleMap = upload(request, response);
+				String title = articleMap.get("title");
+				String content = articleMap.get("content");
+				String imageFileName = articleMap.get("imageFileName");
+				articleVO.setParentNO(0);
+				articleVO.setId("hong");
+				articleVO.setTitle(title);
+				articleVO.setContent(content);
+				articleVO.setImageFileName(imageFileName);
+				articleNO= boardService.addArticle(articleVO);
+				
+				if(imageFileName!=null && imageFileName.length()!=0) {
+				    File srcFile = new 	File(ARTICLE_IMAGE_REPO +"\\"+"temp"+"\\"+imageFileName);
+					File destDir = new File(ARTICLE_IMAGE_REPO +"\\"+articleNO);
+					destDir.mkdirs();
+					FileUtils.moveFileToDirectory(srcFile, destDir, true);
+				}
+				PrintWriter pw = response.getWriter();
+				pw.print("<script>" 
+				         +"  alert('새글을 추가했습니다.');" 
+						 +" location.href='"+request.getContextPath()+"/board/listArticles.do';"
+				         +"</script>");
+
+				return;
+			} else if (action.equals("/viewArticle.do")){
+				// 글 상세창을 요청할 경우 articleNO 값을 가져옴
+				String articleNO = request.getParameter("articleNO");
+				// articleNO에 대한 글 정보를 조회하고 article 속성으로 바인딩
+				articleVO = boardService.viewArticle(Integer.parseInt(articleNO));
+				request.setAttribute("article", articleVO);
+				nextPage = "/board06/viewArticle.jsp";
+			} else if (action.equals("/modArticle.do")) {
+				Map<String, String> articleMap = upload(request, response);
+				int articleNO = Integer.parseInt(articleMap.get("articleNO"));
+				articleVO.setArticleNO(articleNO);
+				String title = articleMap.get("title");
+				String content = articleMap.get("content");
+				String imageFileName = articleMap.get("imageFileName");
+				articleVO.setParentNO(0);
+				articleVO.setId("hong");
+				articleVO.setTitle(title);
+				articleVO.setContent(content);
+				articleVO.setImageFileName(imageFileName);
+				boardService.modArticle(articleVO);
+				if (imageFileName != null && imageFileName.length() != 0) {
+					String originalFileName = articleMap.get("originalFileName");
+					File srcFile = new File(ARTICLE_IMAGE_REPO + "\\" + "temp" + "\\" + imageFileName);
+					File destDir = new File(ARTICLE_IMAGE_REPO + "\\" + articleNO);
+					destDir.mkdirs();
+					FileUtils.moveFileToDirectory(srcFile, destDir, true);
+					;
+					File oldFile = new File(ARTICLE_IMAGE_REPO + "\\" + articleNO + "\\" + originalFileName);
+					oldFile.delete();
+				}
+				PrintWriter pw = response.getWriter();
+				pw.print("<script>" + "  alert('글을 수정했습니다.');" + " location.href='" + request.getContextPath()
+						+ "/board/viewArticle.do?articleNO=" + articleNO + "';" + "</script>");
+				return;	
+			} else if (action.equals("/removeArticle.do")) {
+				int articleNO = Integer.parseInt(request.getParameter("articleNO"));
+				// articleNO 값에 대한 글을 삭제한 후 삭제된 부모 글과 자식 글의 articleNO 목록을 가져옴
+				List<Integer> articleNOList = boardService.removeArticle(articleNO);
+				for (int _articleNO : articleNOList) {
+					File imgDir = new File(ARTICLE_IMAGE_REPO + "\\" + _articleNO);
+					if (imgDir.exists()) {
+						FileUtils.deleteDirectory(imgDir);
+					}
+				}
+				
+				PrintWriter pw = response.getWriter();
+				pw.print("<script>" + " alert('글을 삭제했습니다.');" + " location.href='" 
+						+ request.getContextPath() + "/board/listArticles.do';" + "</script>" );
+				return;
+			} else if (action.equals("/replyForm.do")) {
+				// 답글창 요청 시 미리 부모 글 번호를 parentNO 속성으로 세션에 저장
+				int parentNO = Integer.parseInt(request.getParameter("parentNO"));
+				session = request.getSession();
+				session.setAttribute("parentNO", parentNO);
+				nextPage = "/board06/replyForm.jsp";
+			} else if (action.equals("/addReply.do")) {
+				// 답글 전송 시 세션에 저장된 parentNO 가져옴
+				session = request.getSession();
+				int parentNO = (Integer)session.getAttribute("parentNO");
+				session.removeAttribute("parentNO");
+				
+				Map<String, String> articleMap = upload(request, response);
+				String title = articleMap.get("title");
+				String content = articleMap.get("content");
+				String imageFileName = articleMap.get("imageFileName");
+				// 답글의 부모 글 번호 설정
+				articleVO.setParentNO(parentNO);
+				// 답글 작성자 ID를 lee로 설정
+				articleVO.setId("lee");
+				articleVO.setTitle(title);
+				articleVO.setContent(content);
+				articleVO.setImageFileName(imageFileName);
+				// 답글을 테이블에 추가
+				int articleNO = boardService.addReply(articleVO);
+				if (imageFileName != null && imageFileName.length() != 0) {
+					File srcFile = new File(ARTICLE_IMAGE_REPO + "\\" + "temp" + "\\" + imageFileName);
+					File destDir = new File(ARTICLE_IMAGE_REPO + "\\" + articleNO);
+					destDir.mkdirs();
+					FileUtils.moveFileToDirectory(srcFile, destDir, true);
+				}
+				PrintWriter pw = response.getWriter();
+				pw.print("<script>" + " alert('답글을 추가했습니다.');"
+						+ " location.href='"
+						+ request.getContextPath()
+						+ "/board/viewArticle.do?articleNO="
+						+ articleNO + "';" + "</script>");
+				return;
+			}
+			else {
+				nextPage = "/board06/listArticles.jsp";
+			}
+
+			RequestDispatcher dispatch = request.getRequestDispatcher(nextPage);
+			dispatch.forward(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private Map<String, String> upload(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Map<String, String> articleMap = new HashMap<String, String>();
+		String encoding = "utf-8";
+		File currentDirPath = new File(ARTICLE_IMAGE_REPO);
+		DiskFileItemFactory factory = new DiskFileItemFactory();
+		factory.setRepository(currentDirPath);
+		factory.setSizeThreshold(1024 * 1024);
+		ServletFileUpload upload = new ServletFileUpload(factory);
+		try {
+			List items = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(new ServletRequestContext(request));
+			
+			for (int i = 0; i < items.size(); i++) {
+				FileItem fileItem = (FileItem) items.get(i);
+				if (fileItem.isFormField()) {
+					System.out.println(fileItem.getFieldName() + "=" + fileItem.getString(encoding));
+					articleMap.put(fileItem.getFieldName(), fileItem.getString(encoding));
+				} else {
+					System.out.println("파라미터명:" + fileItem.getFieldName());
+					System.out.println("파일크기:" + fileItem.getSize() + "bytes");
+					if (fileItem.getSize() > 0) {
+						int idx = fileItem.getName().lastIndexOf("\\");
+						if (idx == -1) {
+							idx = fileItem.getName().lastIndexOf("/");
+						}
+
+						String fileName = fileItem.getName().substring(idx + 1);
+						System.out.println("파일명:" + fileName);
+						articleMap.put(fileItem.getFieldName(), fileName); 
+						File uploadFile = new File(currentDirPath + "\\temp\\" + fileName);
+						fileItem.write(uploadFile);
+
+					} // end if
+				} // end if
+			} // end for
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return articleMap;
+	}
+
+}
