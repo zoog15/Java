e6753b1edaa1104f9e596c427bf2bcaea2a77673
KimMinKwan <zoog15@naver.com>
2021-12-01 17:32:08 +0900

    스프링 어노테이션 사용해 URL 요청

---
+++ 자바 웹을 다루는 기술/26장 스프링 어노테이션/com.spring.ex01/MainController.java
@@ -0,0 +1,31 @@
+package com.spring.ex01;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("mainController")
+@RequestMapping("/test")
+public class MainController {
+   @RequestMapping(value="/main1.do" ,method=RequestMethod.GET)
+   public ModelAndView main1(HttpServletRequest request, HttpServletResponse response)  throws Exception{
+	  System.out.println("메인1번");
+      ModelAndView mav=new ModelAndView();
+      mav.addObject("msg","main1");
+      mav.setViewName("main");
+      return mav;
+   }
+
+   @RequestMapping(value="/main2.do" ,method = RequestMethod.GET)
+   public ModelAndView main2(HttpServletRequest request, HttpServletResponse response) throws Exception{
+	   System.out.println("메인2번");
+      ModelAndView mav=new ModelAndView();
+      mav.addObject("msg","main2");
+      mav.setViewName("main");
+      return mav;
+   }
+}
