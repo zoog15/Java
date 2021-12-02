98b017f3d7d8aece8e5a904b9a50aa984d29de0b
KimMinKwan <zoog15@naver.com>
2021-12-02 20:06:00 +0900

    스프링 프로젝트 및 log4j를 이용한 로그 확인

---
+++ 자바 웹을 다루는 기술/27장 메이븐과 스프링 STS/com.spring.pro27/HomeController.java
@@ -0,0 +1,42 @@
+package com.myspring.pro27;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	// 모든 요청에 대해 home() 메서드 호출
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		// 브라우저에서 요청한 시각을 JSP로 전달
+		model.addAttribute("serverTime", formattedDate );
+		
+		// 뷰리졸버로 JSP이름을 반환
+		return "home";
+	}
+	
+}
