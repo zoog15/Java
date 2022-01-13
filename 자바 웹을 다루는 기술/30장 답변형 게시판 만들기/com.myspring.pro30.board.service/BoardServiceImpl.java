4f7749e7a9d9cd2a3ccce72dcfbcbcc8caffe12e
KimMinKwan <zoog15@naver.com>
2022-01-13 16:43:14 +0900

    게시판 글쓰기

---
+++ 자바 웹을 다루는 기술/30장 답변형 게시판 만들기/com.myspring.pro30.board.service/BoardServiceImpl.java
@@ -0,0 +1,53 @@
+package com.myspring.pro30.board.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.myspring.pro30.board.dao.BoardDAO;
+import com.myspring.pro30.board.vo.ArticleVO;
+
+import org.springframework.transaction.annotation.Propagation;
+
+@Service("boardService")
+@Transactional(propagation = Propagation.REQUIRED)
+public class BoardServiceImpl implements BoardService{
+	
+	@Autowired
+	private BoardDAO boardDAO;
+
+	@Override
+	public List<ArticleVO> listArticles() throws Exception {
+		List<ArticleVO> articlesList = boardDAO.selectAllArticlesList();
+		
+		return articlesList;
+	}
+
+	//단일 이미지 추가하기
+	@Override
+	public int addNewArticle(Map articleMap) throws Exception{
+		return boardDAO.insertNewArticle(articleMap);
+	}
+
+	@Override
+	public ArticleVO viewArticle(int articleNO) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void modArticle(Map articleMap) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeArticle(int articleNO) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
